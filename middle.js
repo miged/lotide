de46327c8ac2242eaceb8ce319144bb2a8802dac
Miguel <mige.d@outlook.com>
2021-10-25 21:14:38 -0400

    impl middle

---
+++ middle.js
@@ -0,0 +1,42 @@
+const eqArrays = function(array1, array2) {
+  if (!array1 || !array2 || array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+const middle = function(array) {
+  let newArray = [];
+  if (array.length <= 2) {
+    return newArray;
+  }
+
+  // even arrays
+  if (array.length % 2 === 0) {
+    newArray.push(array[Math.floor(array.length / 2) - 1]);
+  }
+
+  newArray.push(array[Math.floor(array.length / 2)]);
+  return newArray;
+}
+
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
