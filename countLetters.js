a659dd89e349a26349b436d279699b46857a3daa
Miguel <mige.d@outlook.com>
2021-10-26 12:04:00 -0400

    fix countLetters

--- countLetters.js
+++ countLetters.js
@@ -10,14 +10,17 @@ const countLetters = function(string) {
   const results = {};
 
   for (let c of string) {
-    if (results[c]) {
-      results[c] += 1;
-    } else {
-      results[c] = 1;
+    if (c !== ' ') {
+      if (results[c] && c !== ' ') {
+        results[c] += 1;
+      } else {
+        results[c] = 1;
+      }
     }
   }
 
   return results;
 };
 
+assertEqual(countLetters("hello").l, 2);
 console.log(countLetters("hello"));
