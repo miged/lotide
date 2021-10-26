1547b53321133074461245dbbc29f301c7d7e0d0
Miguel <mige.d@outlook.com>
2021-10-26 15:55:46 -0400

    impl map

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+  if (!array1 || !array2 || array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+
+const results2 = map([4, 25, 30, 50, 100], num => num / 2);
+assertArraysEqual(results2, [ 2, 12.5, 15, 25, 50 ]);
