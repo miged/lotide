af4af1ef9fa5b6798806c2a604860d7687802bc4
Miguel <mige.d@outlook.com>
2021-10-28 22:08:41 -0400

    eqArrays: added recursive array comparison

--- eqArrays.js
+++ eqArrays.js
@@ -11,8 +11,13 @@ const eqArrays = function(array1, array2) {
     return false;
   }
 
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+  for (let i in array1) {
+    if (Array.isArray(array1[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      };
+    }
+    else if (array1[i] !== array2[i]) {
       return false;
     }
   }
@@ -24,3 +29,7 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
