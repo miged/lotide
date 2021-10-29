4c3207c35c272afac452d05816a69fac75b7313d
Miguel <mige.d@outlook.com>
2021-10-28 21:58:11 -0400

    eqObjects: added recursive object comparison

--- eqObjects.js
+++ eqObjects.js
@@ -32,6 +32,13 @@ const eqObjects = function(object1, object2) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
+    }
+
+    //check if object
+    if (typeof object1[key] === 'object') {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      };
     } else {
       if (object1[key] !== object2[key]) {
         return false;
@@ -55,3 +62,9 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
