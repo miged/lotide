d2c3c0f025e0d7a2861692fcb39d67eff4c8a03f
Miguel <mige.d@outlook.com>
2021-11-01 00:23:21 -0400

    change more tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("returns [] for [1, 2]", () => {
+    const input = [1, 2];
+    const expected = [];
+    assert.deepEqual(middle(input), expected);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    const input = [1, 2, 3];
+    const expected = [2];
+    assert.deepEqual(middle(input), expected);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    const input = [1, 2, 3, 4];
+    const expected = [2, 3];
+    assert.deepEqual(middle(input), expected);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,16 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-assertEqual(tail(['Hello']).length, 0);
-assertEqual(tail([]).length, 0);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const input = ["Hello", "Lighthouse", "Labs"];
+    const expected = ["Lighthouse", "Labs"];
+    assert.deepEqual(tail(input), expected);
+  });
+  it("returns [] for ['Hello']", () => {
+    assert.deepEqual(tail(['Hello']), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual([], []);
+  });
+});
